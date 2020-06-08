5540afa0b010c43c284b034e75081a3eb5b836e0
Filippos Mikropoulos <filimikr@teiemt.gr>
2020-06-09 01:11:59 +0200

    Adds example of config/keys.js
    Should be renamed to keys.js in the config/ folder

---
+++ config/example.keys.js
@@ -0,0 +1,18 @@
+module.exports = {
+        google: {
+                clientID:'your_clientID_here'
+                clientSecret:'your_clientSecret_here'
+        },
+        facebook: {
+                FACEBOOK_APP_ID:'your_facebook_app_id_here',
+                FACEBOOK_APP_SECRET:'your_facebook_app_secret_here'
+        },
+        github: {
+                clientID: 'your_clientID_here',
+                clientSecret: 'your_clientSecret_here',
+        },
+        twitter: {
+                consumerKey: 'your_consumerKey_here',
+                consumerSecret: 'your_consumerSecret_here',
+        }
+}
